41b954726a989f26dc7d088d2e4af6e2e7e52671
szykac4060 <szymon.kacperski@gmail.com>
2017-02-03 14:48:28 +0100

    Update AppTest.java

--- CERP/src/test/java/tests/CERP/AppTest.java
+++ CERP/src/test/java/tests/CERP/AppTest.java
@@ -4,15 +4,7 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
+asdfasdf
      * @param testName name of the test case
      */
     public AppTest( String testName )
